9bc286ca3c421ceeb7c266a8cb996fa5811ed997
David Faden (revfad.com) <dfaden@gmail.com>
2025-11-01 19:01:21 -0700

    Fix scaling bug
    The previous logic was pretty broken, resulting in wrong dimensions
    in the common case in which height was overly large even after scaling width.
    
    Switch to correct logic where we find the minimum scale factor alpha for which
    alpha * width < max_width and alpha * height < max_height. We can find this
    just by taking the min of max_width / width and max_height / height in the case
    that at least one of the dimensions is overly large. We know that either of these
    would yield the appropriate max within that dimension; by taking the min scaling
    factor, we know that both will then be in bounds.
    
    The logical flaw should've been obvious, but the lack of unit tests helped it
    slip through. TODO: add unit tests.

--- src/main/java/com/revfad/scaleimage/ImageScaler.java
+++ src/main/java/com/revfad/scaleimage/ImageScaler.java
@@ -19,23 +19,19 @@ public class ImageScaler {
         if (width == 0 || height == 0) {
             return image;
         }
-        int newWidth = width;
-        int newHeight = height;
-        if (width > maxWidth) {
-            newWidth = maxWidth;
-            newHeight = Math.max(1, (int) Math.floor((double) maxWidth / width * height));
-            if (newHeight > maxHeight) {
-                newHeight = maxHeight;
-                newWidth = Math.max(1, (int) Math.floor((double) maxHeight / newHeight * newWidth));
-            }
-        }
-        if (newWidth == width && newHeight == height) {
+        if (width > maxWidth || height > maxHeight) {
+            double widthScale = (double) maxWidth / width;
+            double heightScale = (double) maxHeight / height;
+            double scale = Math.min(widthScale, heightScale);
+            int newWidth = Math.max(1, Math.min(maxWidth, (int) (scale * width)));
+            int newHeight = Math.max(1, Math.min(maxHeight, (int) (scale * height)));
+            BufferedImage scaled = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2d = scaled.createGraphics();
+            // TODO: if the following returns false, block until ready or error.
+            graphics2d.drawImage(image, 0, 0, newWidth, newHeight, null);
+            return scaled;
+        } else {
             return image;
         }
-        BufferedImage scaled = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
-        Graphics2D graphics2d = scaled.createGraphics();
-        // TODO: if the following returns false, block until ready or error.
-        graphics2d.drawImage(image, 0, 0, newWidth, newHeight, null);
-        return scaled;
     }
 }
